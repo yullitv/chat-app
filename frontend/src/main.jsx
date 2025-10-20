e48ec6abd0c592a9ac571552470b6473703b1201
yullitv <yuliok03@gmail.com>
2025-10-20 22:59:25 +0300

    test env visibility

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,8 @@ import AuthProvider from "./contexts/Auth/AuthProvider.jsx";
 import ToastProvider from "./contexts/Toast/ToastProvider.jsx";
 import "./styles/index.css";
 
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
