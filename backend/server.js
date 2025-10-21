0834d2049be437b4b99c1e88e31ee968e4674a6e
yullitv <yuliok03@gmail.com>
2025-10-21 11:30:44 +0300

    fix

--- backend/server.js
+++ backend/server.js
@@ -33,10 +33,11 @@ app.use(
       }
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], // ✅ додали PATCH
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 
 app.use(express.json());
 
